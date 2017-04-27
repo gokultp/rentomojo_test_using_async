be369cef9fa1eeca9d5e2a5762e39eed61a095a9
Gokul T P <tp.gokul@gmail.com>
2017-04-27 10:56:50 +0530

    added html parser

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 var async               = require('async');
 
+var Queue               = async.queue(function (data, cb) {
+    console.log('data', data);
+    cb(null, data.url);
+})
+
+
+Queue.push({url: 'google.com'}, function (err, res) {
+    console.log('err, res', err, res)
+});
+
